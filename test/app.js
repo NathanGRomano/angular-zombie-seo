750013170152459497f4793e339b9e5568ce74f2
Nathan G. Romano <nromano@manta.com>
2013-09-04 12:04:50 -0400

    added the node modules and updated the package json file and fixed syntax errors in the test

--- test/app.js
+++ test/app.js
@@ -2,9 +2,9 @@ var assert = require('assert')
   , request = require('supertest')
 	, app = require('../.')
 
-decribe('given the app is running', function () {
+describe('given the app is running', function () {
 
-	describe('when a request is made with ?_escaped_fragment_ in the path' function (done) {
+	describe('when a request is made with ?_escaped_fragment_ in the path', function (done) {
 
 		it('then should return us a page', function (done) {
 
